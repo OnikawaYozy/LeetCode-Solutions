4e80587e5786daa274a6c2b71c9d9594cd956ec3
OnikawaYozy <topaznguyen99@gmail.com>
2026-08-19 17:14:06 +0700

    Add 20_valid_parentheses.cpp - LeetCode Problem 20

---
+++ 20_valid_parentheses.cpp
@@ -0,0 +1,19 @@
+#include <stack>
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (long long i = 0; i < s.size(); i++)
+        {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[') st.push(s[i]);
+            else
+            {
+                if (st.empty()) return false;
+                char top = st.top();
+                if ((s[i] == ')' && top == '(') || (s[i] == '}' && top == '{') || (s[i] == ']' && top == '[')) st.pop();
+                else return false;
+            }
+        }
+        return st.empty();
+    }
+};
